229ac97782d78cec4903b6287b51cfaf788c7a23
Alex Bronfman <abronfman@connectionseducation.com>
2018-04-16 17:40:58 -0400

    Cleaning up

--- ProgrammingTest/Program.cs
+++ ProgrammingTest/Program.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace ProgrammingTest {
     class Program {
         static void Main(string[] args) {
